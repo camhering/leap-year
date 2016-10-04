265203833a9eaf6100026654c355886473dd0041
Cameron Hering and Tyler Ferrin <tyler@>
2016-10-03 19:07:11 -0700

    add all working specs to our program

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,12 @@
 
 var leapYear = function(year) {
-  return false;
+  if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+    return true;
+  } else {
+    return false;
+  }
 };
+
 
 
 $(document).ready(function() {
